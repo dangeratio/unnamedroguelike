f23a7a164457ed16d731601688641f5ce7b768b1
BuildTools <unconfigured@null.spigotmc.org>
2023-07-01 13:51:49 -0400

    bad debugging statements

--- world/world.py
+++ world/world.py
@@ -23,44 +23,66 @@ class World:
         self.biomes = biomes.biomes
 
     def generate(self):
-        # Create a variable to store the world map
-        world_map = np.zeros((self.size, self.size), dtype=int)
+        try:
+            # debug
+            print("Generating world with seed: {}".format(self.seed))
+            # Create a variable to store the world map
+            world_map = np.zeros((self.size, self.size), dtype=int)
+            
+            # Parameters for the Perlin noise
+            scale = 0.1
+            octaves = 6
+            persistence = 0.5
+            lacunarity = 2.0
+            
+            # Generate the world map using Perlin noise
+            for x in range(self.size):
+                for y in range(self.size):
+                    # debug
+                    print("Generating biome for x: {}, y: {}".format(x, y))
+                    try:
+                        noise = pnoise2(x * scale,
+                                        y * scale,
+                                        octaves=octaves,
+                                        persistence=persistence,
+                                        lacunarity=lacunarity,
+                                        repeatx=self.size,
+                                        repeaty=self.size,
+                                        base=self.seed)
+                        
+                        # Select the biome based on the noise value
+                        if noise < -0.2:
+                            # e.g. Industrial Wasteland
+                            world_map[x, y] = 2
+                        elif noise < 0.2:
+                            # e.g. Neon City
+                            world_map[x, y] = 0
+                        else:
+                            # e.g. Hacker's Hideout
+                            world_map[x, y] = 1
+                    
+                    except Exception as e:
+                        # print the error message
+                        print(f"An error occurred while generating noise at position ({x}, {y}): {e}")
+            
+            # debug
+            print("Setting self.map to world_map")
+            # Set the world map to the world object
+            self.map = world_map
+
+            # debug
+            print("World generated successfully")
         
-        # Parameters for the Perlin noise
-        scale = 0.1
-        octaves = 6
-        persistence = 0.5
-        lacunarity = 2.0
-        
-        # Generate the world map using Perlin noise
-        for x in range(self.size):
-            for y in range(self.size):
-                noise = pnoise2(x * scale,
-                                y * scale,
-                                octaves=octaves,
-                                persistence=persistence,
-                                lacunarity=lacunarity,
-                                repeatx=self.size,
-                                repeaty=self.size,
-                                base=self.seed)
-                
-                # Select the biome based on the noise value
-                if noise < -0.2:
-                    # e.g. Industrial Wasteland
-                    world_map[x, y] = 2
-                elif noise < 0.2:
-                    # e.g. Neon City
-                    world_map[x, y] = 0
-                else:
-                    # e.g. Hacker's Hideout
-                    world_map[x, y] = 1
-        
-        # Set the world map to the world object
-        self.map = world_map
-        
+        except Exception as e:
+            # print the error message
+            print("Error generating world: {}".format(e))
+
+        # Return the world object
         return self
 
-    def print(self):
+    def display_world(self):
+        # debug 
+        print("Displaying world map")
         # Prints a grid of biomes in the world
         for x in range(self.size):
             for y in range(self.size):
@@ -73,4 +95,4 @@ class World:
 # Usage example
 world_gen = World(size='small')
 world = world_gen.generate()
-world_gen.print()
+world_gen.display_world()
